c5a9fbb1919ac48439ab1e8a5fbe195c2929b119
hellenringer <helen109618@gmail.com>
2020-12-10 12:30:39 +0400

    initial commit

---
+++ resources/js/app.js
@@ -0,0 +1,16 @@
+require('./bootstrap');
+
+import React from 'react';
+import ReactDOM from 'react-dom' 
+//import App from './Application/App';
+import {BrowserRouter} from "react-router-dom"; 
+
+
+ReactDOM.render(
+    
+<BrowserRouter>
+//<App />,
+</BrowserRouter>,
+<span>дороу</span>,
+document.querySelector('#root')
+);
